8da52a3dc056f16d0d5a027728ef6172bc612e51
Uwe Pries <uwe.pries@digartis.de>
2022-12-16 01:56:59 +0100

    added links to more images

--- src/views/index/index.php
+++ src/views/index/index.php
@@ -10,5 +10,11 @@
     <body>
         <img src="<?php echo $resized_image; ?>"/>
         <img src="<?php echo $cropped_image; ?>"/>
+        <hr>
+        <ol>
+            <li><a href="/cat-323262_1920.jpg/resize/width/320/height/240">Resize (320x240)</a></li>
+            <li><a href="/cat-1045782_1920.jpg/crop/width/640/height/480">Crop (640x480)</a></li>
+            <li><a href="/cat-1045782_1920.jpg/crop/width/640/height/480/x/350/y/180">Crop with offset (640x480+350+180)</a></li>
+        </ol>
     </body>
 </html>
